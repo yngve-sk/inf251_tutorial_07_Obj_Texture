26e762fb067b2b552e14e6b72e4cb4ce2c18684a
Zuzanka <zabka.zuzu007@gmail.com>
2016-11-26 15:11:23 +0100

    temp

---
+++ inf251_tutorial/SingleTextureTerrain.cpp
@@ -0,0 +1,180 @@
+#include "SingleTextureTerrain.h"
+
+SingleTextureTerrain::SingleTextureTerrain(const char* directory,
+	const char* materialDirectory,
+	const char* bumpMap) {
+	loadObject(directory);
+	loadTerrain(materialDirectory);
+	loadBumpMap(bumpMap);
+}
+
+void SingleTextureTerrain::loadObject(const char* directory) {
+
+	if (!model.import(directory)) {
+		cerr << "Error: cannot load model." << endl;
+		return;
+	}
+
+	cout << "Imported model..." << endl;
+
+	// VBO
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER,
+		model.getNumberOfVertices() * sizeof(ModelOBJ::Vertex),
+		model.getVertexBuffer(),
+		GL_STATIC_DRAW);
+
+	// IBO
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		model.getNumberOfIndices() * sizeof(unsigned int),
+		model.getIndexBuffer(),
+		GL_STATIC_DRAW);
+
+	model.normalize();
+
+	return;
+}
+
+void SingleTextureTerrain::loadTerrain(const char* textureDirectory) {
+
+	unsigned char* TextureData = nullptr;
+	unsigned int TextureWidth = 0;
+	unsigned int TextureHeight = 0;
+
+	cout << "loading material: " << textureDirectory << endl;
+
+	// Load the texture
+	if (TextureData != nullptr)
+		free(TextureData);
+
+	//cout << "round " << i << " trying to load @ file path " << "House-Model\\" << model.getMaterial(i).colorMapFilename.c_str() << endl;
+	unsigned int fail = lodepng_decode_file(&TextureData, &TextureWidth, &TextureHeight,
+		textureDirectory,
+		LCT_RGB,
+		8); // Remember to check the last 2 parameters
+	if (fail != 0) {
+		cerr << "Error: cannot load texture file "
+			<< textureDirectory << endl;
+		return;
+	}
+
+	// Create the texture object
+	if (textureObject != 0)
+		glDeleteTextures(1, &textureObject);
+	glGenTextures(1, &textureObject);
+
+	// Bind it as a 2D texture (note that other types of textures are supported as well)
+	glBindTexture(GL_TEXTURE_2D_ARRAY, textureObject);
+
+	// Set the texture data
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		GL_RGB,			// remember to check this
+		TextureWidth,
+		TextureHeight,
+		0,
+		GL_RGB,			// remember to check this
+		GL_UNSIGNED_BYTE,
+		TextureData
+		);
+
+	// Configure texture parameter
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void SingleTextureTerrain::loadBumpMap(const char* textureDirectory) {
+
+	unsigned char* TextureData = nullptr;
+	unsigned int TextureWidth = 0;
+	unsigned int TextureHeight = 0;
+
+	cout << "loading material: " << textureDirectory << endl;
+
+	// Load the texture
+	if (TextureData != nullptr)
+		free(TextureData);
+
+	//cout << "round " << i << " trying to load @ file path " << "House-Model\\" << model.getMaterial(i).colorMapFilename.c_str() << endl;
+	unsigned int fail = lodepng_decode_file(&TextureData, &TextureWidth, &TextureHeight,
+		textureDirectory,
+		LCT_RGB,
+		8); // Remember to check the last 2 parameters
+	if (fail != 0) {
+		cerr << "Error: cannot load texture file "
+			<< textureDirectory << endl;
+		return;
+	}
+
+	// Create the texture object
+	if (bumpMapObject != 0)
+		glDeleteTextures(1, &bumpMapObject);
+	glGenTextures(1, &bumpMapObject);
+
+	// Bind it as a 2D texture (note that other types of textures are supported as well)
+	glBindTexture(GL_TEXTURE_2D_ARRAY, bumpMapObject);
+
+	// Set the texture data
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		GL_RGB,			// remember to check this
+		TextureWidth,
+		TextureHeight,
+		0,
+		GL_RGB,			// remember to check this
+		GL_UNSIGNED_BYTE,
+		TextureData
+		);
+
+	// Configure texture parameter
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
+
+void SingleTextureTerrain::drawObject(VertexGLLocs vertexGLLocs, MaterialGLLocs materialGLLocs) {
+
+	ModelOBJ::Material material = model.getMaterial(0);
+
+	// Set material parameters for house
+	glUniform3f(materialGLLocs.aColorLoc, material.ambient[0], material.ambient[1], material.ambient[2]);
+	glUniform3f(materialGLLocs.dColorLoc, material.diffuse[0], material.diffuse[1], material.diffuse[2]);
+	glUniform3f(materialGLLocs.sColorLoc, material.specular[0], material.specular[1], material.specular[2]);
+	glUniform1f(materialGLLocs.shineLoc, material.shininess);
+
+	glActiveTexture(GL_TEXTURE0);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, textureObject);
+
+	glActiveTexture(GL_TEXTURE1);
+	glEnable(GL_TEXTURE_2D);
+	//int normal_location = glGetUniformLocation(ShaderProgram, "normal_texture");
+	//glUniform1i(normal_location, 1);
+	glBindTexture(GL_TEXTURE_2D, bumpMapObject);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glVertexAttribPointer(vertexGLLocs.posLoc, 3, GL_FLOAT, GL_FALSE,
+		sizeof(ModelOBJ::Vertex), reinterpret_cast<const GLvoid*>(0));
+	glVertexAttribPointer(vertexGLLocs.texLoc, 2, GL_FLOAT, GL_FALSE,
+		sizeof(ModelOBJ::Vertex), reinterpret_cast<const GLvoid*>(3 * sizeof(float)));
+	if (vertexGLLocs.normalLoc != -1) {
+		glVertexAttribPointer(vertexGLLocs.normalLoc, 3, GL_FLOAT, GL_FALSE,
+			sizeof(ModelOBJ::Vertex), reinterpret_cast<const GLvoid*>(5 * sizeof(float)));
+	}
+	glDrawElements(GL_TRIANGLES, model.getNumberOfIndices(), GL_UNSIGNED_INT, 0);
+
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_TEXTURE_2D);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_TEXTURE_2D);
+}
+
+
--- inf251_tutorial/SingleTextureTerrain.h
+++ inf251_tutorial/SingleTextureTerrain.h
@@ -1,1 +1,43 @@
-#pragma once
+#include <gl/glew.h>
+#include <gl/glut.h>
+#include <gl/GL.h>
+
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/extend.hpp>
+
+#include "model_obj.h"
+#include "lodepng.h"
+#include "GLLocStructs.h"
+#include "Transformation.h"
+
+using namespace std;
+using namespace glm;
+
+class SingleTextureTerrain {
+
+public:
+	ModelOBJ model;
+
+	GLuint VBO = -1;
+	GLuint IBO = -1;
+
+	GLuint textureObject;
+	GLuint bumpMapObject;
+
+	Transformation transformation;
+
+	bool usingBumpMapping;
+
+	void drawObject(VertexGLLocs, MaterialGLLocs);
+	void loadObject(const char*);
+	void loadTerrain(const char*);
+	void loadBumpMap(const char*);
+
+	SingleTextureTerrain(const char*, const char*, const char*);
+
+private:
+};
